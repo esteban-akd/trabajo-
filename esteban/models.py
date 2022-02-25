bd15cdbe3c2b33105475b78b9f7840d29e4d4cf6
esteban-akd <93355509+esteban-akd@users.noreply.github.com>
2022-02-24 21:09:36 -0300

    Update and rename Esteban tp/models.py to esteban/models.py

---
+++ esteban/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Persona(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=60)
+    apellido = models.CharField(max_length=100)
+    dni=models.CharField(max_length=8)
+    fecha=models.DateField(auto_now=True)
+    club = models.CharField(max_length=50)
+    foto = models.ImageField(
+        upload_to = 'foto/%Y/%m/%d',
+        blank = True,
+        verbose_name = ('Foto de la persona')
+    )
+  
+    
+
+class Meta:
+    verbose_name = ('Persona')
+    verbose_name_plural = ('Persona')
+    
+    
+
+def _str_(self):
+    return self.
